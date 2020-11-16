73a20bcdcc847be3170261ae90da0c566b322c1b
CadenCGS <cserrato21352@columbushs.com>
2020-11-16 11:05:18 -0500

    added arrays in 6

---
+++ exercize6.js
@@ -0,0 +1,36 @@
+// using this array,
+// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+// 1. Remove the Banana from the array.
+
+var array = ["Apples", "Oranges", "Blueberries"];
+
+// 2. Sort the array in order.
+
+var array = ["Apples", "Blueberries", "Oranges"];
+
+// 3. Put "Kiwi" at the end of the array.
+
+var array = ["Apples", "Blueberries", "Oranges", "Kiwi"];
+
+// 4. Remove "Apples" from the array.
+
+var array = ["Blueberries", "Oranges", "Kiwi"];
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+
+var array = ["Kiwi", "Oranges", "Blueberries"];
+
+// you should have at the end:
+// ["Kiwi", "Oranges", "Blueberries"]
+
+// using this array,
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+
+var array2 = ["Banana", ["Apples"], ["Oranges"], "Blueberries"];
+array2[1][1] 
+//or
+array2[1][1][0]
